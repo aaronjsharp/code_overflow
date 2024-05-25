53597e800dc7d99034a6e7964d94b70e28908287
aaronjsharp <aaron.sharp2011@gmail.com>
2024-05-25 15:06:18 -0500

    Add theme context for light/dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,64 @@
-import React from 'react'
-import { Inter, Space_Grotesk } from 'next/font/google'
-import type { Metadata } from 'next'
+import React from 'react';
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
 
 import {
   ClerkProvider,
   SignInButton,
   SignedIn,
   SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+  UserButton,
+} from '@clerk/nextjs';
 
-import './globals.css'
+import './globals.css';
+import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
-})
+  variable: '--font-inter',
+});
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk'
-})
+  variable: '--font-spaceGrotesk',
+});
 
 export const metadata: Metadata = {
   title: 'CodeFlow',
-  description: 'A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+  description:
+    'A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
   icons: {
-    icon: '/assets/images/site-logo.svg'
-  }
-}
+    icon: '/assets/images/site-logo.svg',
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover:text-primary-500'
-        }
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          <SignedOut>
+    <html lang='en'>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500',
+            },
+          }}
+        >
+          {/* <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
-          </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+          </SignedIn> */}
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
