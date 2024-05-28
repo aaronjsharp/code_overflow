5e0940daaf1ef3d3f7d2f83a14ab591fbd0df556
aaronjsharp <aaron.sharp2011@gmail.com>
2024-05-28 08:49:09 -0500

    Add left sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 export const metadata: Metadata = {
-  title: 'CodeFlow',
+  title: 'CodeOverflow.com',
   description:
     'A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
   icons: {
