c30a9a4b2e8485bc8202e2479ef70cf0d99dfc66
aaronjsharp <aaron.sharp2011@gmail.com>
2024-05-28 13:48:41 -0500

    Add HomeFilters and NoResults components

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,25 +1,73 @@
-import Filter from "@/components/shared/Filter";
-import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
-import { Button } from "@/components/ui/button";
-import { HomePageFilters } from "@/constants/filters";
-import Link from "next/link";
+import HomeFilters from '@/components/home/HomeFilters';
+import Filter from '@/components/shared/Filter';
+import NoResult from '@/components/shared/NoResult';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import { Button } from '@/components/ui/button';
+import { HomePageFilters } from '@/constants/filters';
+import Link from 'next/link';
+
+const questions = [
+  // {
+  //   _id: 1,
+  //   title: 'How do I write SQL?',
+  //   tags: [
+  //     {
+  //       _id: 1,
+  //       name: 'sql',
+  //     },
+  //   ],
+  //   author: 'John Doe',
+  //   upvotes: 10,
+  //   views: 100,
+  //   answers: 3,
+  //   createdAt: '2021-09-01T12:00:00.000Z',
+  // },
+  // {
+  //   _id: 2,
+  //   title: 'How does React work?',
+  //   tags: [
+  //     {
+  //       _id: 2,
+  //       name: 'react',
+  //     },
+  //   ],
+  //   author: 'Sally Doe',
+  //   upvotes: 10,
+  //   views: 50,
+  //   answers: 17,
+  //   createdAt: '2021-09-01T12:00:00.000Z',
+  // },
+  // {
+  //   _id: 3,
+  //   title: 'How does Vue work?',
+  //   tags: [
+  //     {
+  //       _id: 3,
+  //       name: 'vue',
+  //     },
+  //   ],
+  //   author: 'John Smith',
+  //   upvotes: 10,
+  //   views: 99,
+  //   answers: 10,
+  //   createdAt: '2021-09-01T12:00:00.000Z',
+  // },
+];
 
 export default function Home() {
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">
-          All Questions
-        </h1>
+      <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+        <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
 
-        <Link href='/ask-question' className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+        <Link href='/ask-question' className='flex justify-end max-sm:w-full'>
+          <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900'>
             Ask a Question
           </Button>
         </Link>
       </div>
 
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
         <LocalSearchbar
           route='/'
           iconPosition='left'
@@ -28,12 +76,25 @@ export default function Home() {
           otherClasses='flex-1'
         />
 
-        <Filter 
+        <Filter
           filters={HomePageFilters}
           otherClasses='min-h-[56px] sm:min-w-[170px]'
           containerClasses='hidden max-md:flex'
         />
       </div>
+
+      <HomeFilters />
+
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        {questions.length > 0
+          ? questions.map((question) => 'QuestionCard')
+          : <NoResult 
+            title="There's no question to show"
+            description='Be the first to break the silence! Ask a question and kickstart the discussion. Your question could be the next big thing others learn from. Get involved!'
+            link='/ask-question'
+            linkTitle='Ask a Question'
+            />}
+      </div>
     </>
-  )
+  );
 }
