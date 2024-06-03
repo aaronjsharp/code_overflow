7be1652aa47249df99aa622a2113d78e943489fa
aaronjsharp <aaron.sharp2011@gmail.com>
2024-06-03 10:34:19 -0500

    Complete saved questions page

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -17,7 +17,7 @@ interface QuestionCardProps {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -69,7 +69,7 @@ const QuestionCard = ({
         <Metric
           imgUrl='/assets/icons/like.svg'
           alt='upvotes'
-          value={formatNumber(upvotes)}
+          value={formatNumber(upvotes.length)}
           title=' Votes'
           textStyles='small-medium text-dark400_light800'
         />
