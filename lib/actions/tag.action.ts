bc44952f0d0e9c34a5d36d20d9ece291b7ec1e69
aaronjsharp <aaron.sharp2011@gmail.com>
2024-07-02 16:39:38 -0500

    Fix error causing deployment issue

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -96,7 +96,7 @@ export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
   try {
     connectToDatabase();
 
-    const { tagId, page = 1, pageSize = 1, searchQuery } = params;
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
 
     const skipAmount = (page - 1) * pageSize
 
